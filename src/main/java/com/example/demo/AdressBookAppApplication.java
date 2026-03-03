c2c7bdc40ff3bf52301eba81805790ed24fca52a
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-03-03 14:18:46 +0530

    [Himesh] : added Initial Spring boot setup

---
+++ src/main/java/com/example/demo/AdressBookAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdressBookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdressBookAppApplication.class, args);
+	}
+
+}
