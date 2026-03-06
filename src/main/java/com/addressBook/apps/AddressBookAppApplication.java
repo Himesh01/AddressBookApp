e9bd9c4f7ebe849fca3d68ef282727b78afa17f2
Himesh Kurmi <rajendrakurmi729@gmail.com>
2026-03-06 09:29:58 +0530

    [Himesh] : added UC2-Add Contacts files

--- src/main/java/com/addressBook/apps/AddressBookAppApplication.java
+++ src/main/java/com/addressBook/apps/AddressBookAppApplication.java
@@ -8,6 +8,6 @@ public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressBookAppApplication.class, args);
+		
 	}
-
 }
--- src/test/java/com/addressBook/apps/AddressBookAppApplicationTests.java
+++ src/test/java/com/addressBook/apps/AddressBookAppApplicationTests.java
@@ -4,7 +4,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AddressBookAppApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
